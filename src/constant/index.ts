4bca695b9527beb6f623752fab15b8eeaae29c9f
snowye7 <601994226@qq.com>
2024-04-25 14:12:06 +0800

    ip

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,1 +1,1 @@
-export const baseUrl = import.meta.env.MODE === "development" ? "http://192.168.1.118:8888" : "http://127.0.0.1:8888"
+export const baseUrl = import.meta.env.MODE === "development" ? "http://192.168.1.136:8888" : "http://127.0.0.1:8888"
